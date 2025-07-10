20bf6073952dfd58c907e7d130fe75f513876d56
Alejandro-S-M <alejandro.sanchez.mejorada@gmail.com>
2025-07-10 11:53:08 -0400

    add asteroids and movement

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
+from asteroid import *
+from asteroidfield import *
 
 pygame.init()
 
@@ -12,8 +16,18 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    #Organizing by group
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     #Initate Graphic User Interface
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     # Clock game
     fps_clock = pygame.time.Clock()
@@ -28,9 +42,13 @@ def main():
         
         # Drawing second
         screen.fill("black")
+        # Hooking rotation
+        updatable.update(dt) 
+        # Render player
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        fps_clock.tick(60)
         dt = fps_clock.tick(60)/1000
-
+       
 if __name__ == "__main__":
     main()
